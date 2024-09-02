65651a2e821d3f58ae6ac29324c3eb93a34f5027
Uchenna Ofor <ucheofor2015@gmail.com>
2024-09-02 14:11:15 +0100

    corsjpdated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
